4a490985b5e131151b5f1faecb66262155b77f1d
Jhila Ferdows <jhila.ferdows@menloschool.org>
2021-10-11 14:06:42 -0700

    Dictionaries Exercises

---
+++ project0/mtnHeights.py
@@ -0,0 +1,10 @@
+mtns = {"Mount Everst" : 8848,
+        "Kangchenjunga" : 8586,
+        "Lhotse" : 8516,
+        "Makalu" : 8485,
+        "Cho Oyu" : 8188,
+        }
+for x, y in mtns.items():
+    print("Mountain: %s" % x)
+    print("Height %s" % y)
+    print()
---
+++ project0/petNames.py
@@ -0,0 +1,4 @@
+pet_names = {"zoe" : "dog", "max" : "cat", "zach" : "parrot", "ella" : "pig"}
+
+for x, y in pet_names.items():
+    print(x, " is a ", y)
