1019843009eb2d0bd3259e93d4ac4fec7728a27e
Jhila Ferdows <jhila.ferdows@menloschool.org>
2022-04-29 10:01:30 -0700

    load maze, fill maze with cells, print maze

--- S2_FinalProject/cell.py
+++ S2_FinalProject/cell.py
@@ -1,21 +1,8 @@
-from mazebot import mazebot
 
-class cell:
+class Cell:
     def __init__(self):
-        mb = mazebot()
+        self.visted = None
+        self.wall = None
+        self.parent = None
 
-        self.x = mazebot.self.x
-        self.y = mazebot.self.x
-        self.curr_cell = None
-
-        self.visited = []
-
-    def current_cell (self):
-        if (self.y == 1):
-            self.curr_cell = self.x-1
-        elif(self.y == 2):
-            self.curr_cell = 5+1
-        elif (self.y == 3):
-            self.curr_cell = 8+1
-        self.visited.add(self.curr_cell)
 
--- S2_FinalProject/mazebot.py
+++ S2_FinalProject/mazebot.py
@@ -1,5 +1,6 @@
+from cell import Cell
 
-class mazebot:
+class Mazebot:
     def __init__(self):
         # location of the boat
         self.x = 1
@@ -8,11 +9,63 @@ class mazebot:
         self.end_x = 3
         self.end_y = 6
 
-        self.current_cell = []*2
-        self.visited_cells = []*2
+        self.current_cell = None
+        self.poss_cells = []
 
-        # location of maze
-        self.maze = None
+        # array of maze
+        self.maze = []
+
+    # load the maze file
+    # fill maze with cells
+    def load_maze(self, file):
+        int_maze = []
+        with open(file) as f:
+            line = f.readline().strip()
+            while line:
+                line = [int(char) for char in line]
+                int_maze.append(line)
+                line = f.readline().strip()
+        # print(int_maze)
+
+        self.fill_maze_cells(int_maze)
+
+
+    # determining what spots in array are walls
+    # adding cells to maze
+    def fill_maze_cells(self, int_maze):
+        # int_maze = mazebot.load_maze("maze1.txt")
+        for row in range(len(int_maze)):
+            temp_row = []
+            for col in range(len(int_maze[0])):
+                cell = Cell()
+                if (int_maze[row][col] == 1):
+                    cell.wall = True
+                else:
+                    cell.wall = False
+                temp_row.append(cell)
+            self.maze.append(temp_row)
+
+
+    # def is_cell_wall(self, cell, row, col):
+    #     if (self.maze[row][col] == 1):
+    #         return True
+    #     else:
+    #         return False
+
+    # print maze
+    def print_maze(self):
+        for row in range(len(self.maze)):
+            for col in range(len(self.maze[0])):
+                cell = self.maze[row][col]
+                if (cell.wall == True):
+                    print("1", end="")
+                else:
+                    print("0", end="")
+            print("")
+
+
+
+
 
     def move_right(self, node):
         self.x += 1
@@ -40,5 +93,10 @@ class mazebot:
 
     def minimax(self):
         self.move_right()
+
+if __name__ == '__main__':
+    mazebot = Mazebot()
+    mazebot.load_maze("maze1.txt")
+    mazebot.print_maze()
 
 
